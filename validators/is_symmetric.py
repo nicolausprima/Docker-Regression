38c3a8c0c0ab776b397d2478c67cbc09cf7b9e86
nicolausprima <nicolausprimaa@gmail.com>
2025-09-06 12:32:34 +0000

    Del

--- validators/is_symmetric.py
+++ validators/is_symmetric.py
@@ -1,14 +1,13 @@
 def is_symmetric(matrix):
     """
     Mengecek apakah sebuah matriks simetris.
-    Matriks simetris berarti matrix[i][j] == matrix[j][i] untuk semua elemen.
     """
-    if matrix.rows != matrix.cols:
+    if not is_square(matrix):
         return False
 
-    for i in range(matrix.rows):
-        for j in range(matrix.cols):
-            if matrix.data[i][j] != matrix.data[j][i]:
+    n = len(matrix)
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] != matrix[j][i]:
                 return False
-
     return True
